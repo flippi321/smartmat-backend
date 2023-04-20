50c5d303307dd464bba9a8299c84936be5602e8e
Håkon Bergrem <haakohbe@stud.ntnu.no>
2023-04-20 14:12:06 +0200

    Added test-classes and GroceryItemFridgeRepository

--- src/main/java/edu/ntnu/idatt2106_09/backend/repository/GroceryItemFridgeRepository.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/repository/GroceryItemFridgeRepository.java
@@ -1,10 +1,9 @@
 package edu.ntnu.idatt2106_09.backend.repository;
 
-import edu.ntnu.idatt2106_09.backend.model.Fridge;
+import edu.ntnu.idatt2106_09.backend.model.GroceryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FridgeRepository extends JpaRepository<Fridge, Long> {
-
+public interface GroceryItemFridgeRepository extends JpaRepository<GroceryItem, Long> {
 }
