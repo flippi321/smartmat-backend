6ae3821957cd3496a17bb885c5bd729650ba3e5b
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-20 08:55:26 +0200

    Started implementing logic for model classes

--- src/main/java/edu/ntnu/idatt2106_09/backend/model/Fridge.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/model/Fridge.java
@@ -1,5 +1,20 @@
 package edu.ntnu.idatt2106_09.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Fridge")
 public class Fridge {
-    private String test = "test";
+    @EmbeddedId
+    private FridgeId fridgeId;
+
+    @ManyToOne
+    @MapsId("householdId")
+    @JoinColumn(name = "household_id")
+    private Household household;
+
 }
--- src/main/java/edu/ntnu/idatt2106_09/backend/model/HouseHold.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/model/HouseHold.java
@@ -1,4 +1,25 @@
 package edu.ntnu.idatt2106_09.backend.model;
 
-public class HouseHold {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Household")
+public class Household {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "household_id")
+    private int householdId;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "household", cascade = CascadeType.ALL)
+    private List<Fridge> fridges;
 }
