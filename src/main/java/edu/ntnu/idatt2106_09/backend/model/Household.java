b12c78dd399bd018d55c6cde912271390081aba7
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-21 16:28:53 +0200

    Changes id from type int to type Long

--- src/main/java/edu/ntnu/idatt2106_09/backend/model/Household.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/model/Household.java
@@ -2,10 +2,12 @@ package edu.ntnu.idatt2106_09.backend.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "household")
 public class Household {
@@ -13,7 +15,7 @@ public class Household {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "household_id")
-    private int householdId;
+    private Long householdId;
 
     @Column(name = "name")
     private String name;
