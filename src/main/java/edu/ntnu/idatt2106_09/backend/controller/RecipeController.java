05419b238a7e5c2614549239ac95f4cb2883500d
Arnold <arnold01@live.no>
2023-04-24 15:33:59 +0200

    Added RecipeController with some CRUD methods and RecipeDTO class

---
+++ src/main/java/edu/ntnu/idatt2106_09/backend/controller/RecipeController.java
@@ -0,0 +1,65 @@
+package edu.ntnu.idatt2106_09.backend.controller;
+
+import edu.ntnu.idatt2106_09.backend.dto.RecipeDTO;
+import edu.ntnu.idatt2106_09.backend.exceptionHandling.NotFoundException;
+import edu.ntnu.idatt2106_09.backend.model.Fridge;
+import edu.ntnu.idatt2106_09.backend.model.Recipe;
+import edu.ntnu.idatt2106_09.backend.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/recipes")
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+
+
+    @PostMapping
+    public ResponseEntity<RecipeDTO> addRecipe(@RequestBody RecipeDTO recipeDTO) {
+
+        Recipe recipe = modelMapper.map(recipeDTO, Recipe.class);
+        recipeService.addRecipe(recipe);
+        return new ResponseEntity<>(recipeDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{recipeId}")
+    public ResponseEntity<RecipeDTO> getRecipeById(@PathVariable Long recipeId) {
+        log.debug("Fetching recipe with id: {}", recipeId);
+        Recipe recipe = recipeService.getRecipeById(recipeId)
+                .orElseThrow(() -> new NotFoundException("recipe with id " + recipeId + " not found"));
+        log.debug("Recipe with id {} found", recipeId);
+        RecipeDTO recipeDTO = modelMapper.map(recipe, RecipeDTO.class);
+        return new ResponseEntity<>(recipeDTO, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<RecipeDTO>> getAllRecipes() {
+        Set<Recipe> recipes = recipeService.getAllRecipe();
+        Set<RecipeDTO> recipeDTOs = recipes.stream()
+                .map(recipe -> modelMapper.map(recipe, RecipeDTO.class))
+                .collect(Collectors.toSet());
+        return new ResponseEntity<>(recipeDTOs, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{recipeId}")
+    public ResponseEntity<Void> deleteRecipe(@PathVariable Long recipeId) {
+        recipeService.deleteRecipe(recipeId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
