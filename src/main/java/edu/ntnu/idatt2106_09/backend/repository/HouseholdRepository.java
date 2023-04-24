222fd5d5efac49513bf4dec6ff79ed960e6bd94d
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-24 14:11:21 +0200

    Added method for retrieving a household from the service

--- src/main/java/edu/ntnu/idatt2106_09/backend/repository/HouseholdRepository.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/repository/HouseholdRepository.java
@@ -1,5 +1,5 @@
 package edu.ntnu.idatt2106_09.backend.repository;
-/*
+
 import edu.ntnu.idatt2106_09.backend.model.Household;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HouseholdRepository extends JpaRepository<Household, Long> {
 }
-*/
 
