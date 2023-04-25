13a594f05329c11c947a8e7502e75fce3f9386da
Arnold <arnold01@live.no>
2023-04-25 13:27:30 +0200

    Implement recipe recommendation with expiration-based weighting:   - Added compareFridgeAndRecipeList method to calculate the ratio of matching fridge and recipe grocery items   - Implement getRecipesOverThreshold to filter recipes based on a predefined threshold   - Added WeightList method to assign weights to recipes considering the expiration dates of groceries   - Use Quick Sort to sort the recipes based on their weights in descending order

--- src/main/java/edu/ntnu/idatt2106_09/backend/service/RecipeService.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/service/RecipeService.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2106_09.backend.service;
 
+import edu.ntnu.idatt2106_09.backend.config.RecipeRecommenderConstants;
 import edu.ntnu.idatt2106_09.backend.dto.*;
 
 import edu.ntnu.idatt2106_09.backend.model.*;
@@ -52,14 +53,9 @@ public class RecipeService {
      recipeRepository.deleteById(recipeId);
     }
 
-    // 1. Retrive fridge items
-    // 2. Retrive Recipe List
-    // 4. Compute Best recipes based on items in fridge
-    // 5. Compute recipe list based on exp date
 
-
-    private HashMap<String, GroceryItemFridgeDTO> retrieveFridgeItemsHashMap(long fridgeId) {
-        HashMap<String, GroceryItemFridgeDTO> map = new HashMap<>();
+    private HashMap<Long, GroceryItemFridgeDTO> retrieveFridgeItemsHashMap(long fridgeId) {
+        HashMap<Long, GroceryItemFridgeDTO> map = new HashMap<>();
 
         Fridge fridge = fridgeRepository.findById(fridgeId).get();
         FridgeDTO fridgeDTO = fridgeService.castFridgeDTO(fridge);
@@ -78,16 +74,14 @@ public class RecipeService {
             currentGroceryItemFridgeDTO.setGroceryItem(groceryItemService.castGroceryItemDto(gif.getGroceryItem()));
 
 
-            map.put(gif.getGroceryItem().getName(), currentGroceryItemFridgeDTO);
+            map.put(gif.getGroceryItem().getGroceryItemId(), currentGroceryItemFridgeDTO);
         }
 
         return map;
     }
 
 
-
-
-    private List<List<GroceryItemRecipeDTO>> getRecipeList() {
+    private List<List<GroceryItemRecipeDTO>> getAllRecipeList() {
 
         Set<Recipe> allRecipes = recipeRepository.getAllRecipes();
 
@@ -128,57 +122,145 @@ public class RecipeService {
         return GroceryItemRecipeDTOList;
     }
 
-    private void compareFridgeAndRecipeList(){
 
-    }
+    /**
+     * Compares the contents of a HashMap representing the fridge to a list of grocery items required to make a recipe,
+     * and returns a double value between 0 and 1 indicating the level of matching between the two.
+     * This method calculates the ratio of the number of grocery items required for the recipe that are present in the fridge,
+     * to the total number of grocery items required for the recipe.
+     * @return a double value between 0 and 1 representing the level of matching between the fridge contents and the recipe
+     */
+    private double compareFridgeAndRecipeList
+            (HashMap<Long, GroceryItemFridgeDTO> fridge, List<GroceryItemRecipeDTO> recipe) {
+        GroceryItemRecipeDTO recipeItem;
+        double fridgeItemAmount;
+        double currentPercentage = 0.0;
+        double ingredientPercentage;
 
-    private void WeightListBasedOnExpirationDate() {
 
-    }
+        for(int i = 0; i<recipe.size(); i++){
+            ingredientPercentage = 0.0;
+            recipeItem = recipe.get(i);
 
-    /*
-    Bubble sort needs to be modified
 
-    public static void quickSort(double[] arr, ArrayList<ArrayList<Item>> recipes, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(arr, recipes, low, high); // Partition the array
-            quickSort(arr, recipes, low, pivotIndex - 1);  // Recursively sort the left subarray
-            quickSort(arr, recipes, pivotIndex + 1, high); // Recursively sort the right subarray
+            if(fridge.get(recipeItem.getGroceryItem().getGroceryItemId()) != null){
+                fridgeItemAmount = fridge.get(recipeItem.getGroceryItem().getGroceryItemId()).getAmount();
+
+                // Percentage calculation (Maybe change later)
+                if(fridgeItemAmount>=recipeItem.getAmount()) fridgeItemAmount = recipeItem.getAmount();
+
+                ingredientPercentage = fridgeItemAmount / recipeItem.getAmount();
+                currentPercentage += ingredientPercentage;
+            }
+
         }
 
+        return currentPercentage/(recipe.size());
     }
 
-    private static int partition(double[] arr, ArrayList<ArrayList<Item>> recipes, int low, int high) {
-        double pivot = arr[high];
-        int i = low - 1; // Index of smaller element
+    /**
+     * Filters the recipes based on the comparison between fridge contents and recipe grocery items. Only returns recipes
+     * that meet or exceed the defined threshold in RecipeRecommenderConstants.RECOMMENDATION_THRESHOLD.
+     *
+     * @param fridge A Hashtable containing the contents of the fridge with key as GroceryItem ID and value as GroceryItemFridgeDTO.
+     * @param recipeList A list of lists of GroceryItemRecipeDTO objects representing the recipes and their required grocery items.
+     * @return A list of lists of GroceryItemRecipeDTO objects representing the filtered recipes that meet or exceed the threshold.
+     */
+
+
+    private List<List<GroceryItemRecipeDTO>> getRecipesOverThreshold(
+                    HashMap<Long, GroceryItemFridgeDTO> fridge,
+                    List<List<GroceryItemRecipeDTO>> recipeList) {
+
+        List<List<GroceryItemRecipeDTO>> recipesAboveTheThreshold = new ArrayList<>();
+
+        for(int i = 0; i<recipeList.size(); i++){
+            if(compareFridgeAndRecipeList(fridge, recipeList.get(i))>= RecipeRecommenderConstants.RECOMMENDATION_THRESHOLD)
+                recipesAboveTheThreshold.add(recipeList.get(i));
+        }
+
+        if(recipeList.isEmpty()) return null;
+        return recipesAboveTheThreshold;
+
+    }
+
+
+
+
+    /**
+     * Calculates the weight of each recipe in the recipesOverThreshold list based on the available fridge items.
+     * The weight represents how closely the fridge items match the recipe's required grocery items.
+     *
+     * @param fridgeMap A HashMap containing the contents of the fridge with key as GroceryItem ID and value as GroceryItemFridgeDTO.
+     * @param recipesOverThreshold A list of lists of GroceryItemRecipeDTO objects representing the filtered recipes that meet or exceed the threshold.
+     * @return A double array containing the weight of each recipe in the recipesOverThreshold list.
+     */
+    private double[] WeightList(HashMap<Long, GroceryItemFridgeDTO> fridgeMap, List<List<GroceryItemRecipeDTO>> recipesOverThreshold) {
+        int numRecipes = recipesOverThreshold.size();
+        double[] weights = new double[numRecipes];
+
+        for (int i = 0; i < numRecipes; i++) {
+            List<GroceryItemRecipeDTO> recipe = recipesOverThreshold.get(i);
+            double weight = compareFridgeAndRecipeList(fridgeMap, recipe);
+            weights[i] = weight;
+        }
+
+        return weights;
+    }
+
+
+    /**
+     * Sorts the double[] weight and List<List<GroceryItemRecipeDTO>> recipe in descending order based on the values in weight
+     * using the Quick Sort algorithm.
+     *
+     * @param weight A double array containing the weight of each recipe in the recipes list.
+     * @param recipes A list of lists of GroceryItemRecipeDTO objects representing the recipes to be sorted.
+     * @param low The starting index of the subarray to be sorted.
+     * @param high The ending index of the subarray to be sorted.
+     */
+    public void quickSort(double[] weight, List<List<GroceryItemRecipeDTO>> recipes, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(weight, recipes, low, high);
+            quickSort(weight, recipes, low, pivotIndex - 1);
+            quickSort(weight, recipes, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(double[] weight, List<List<GroceryItemRecipeDTO>> recipes, int low, int high) {
+        double pivot = weight[high];
+        int i = low - 1;
+
         for (int j = low; j < high; j++) {
-            // If current element is greater than or equal to pivot
-            if (arr[j] >= pivot) {
+            if (weight[j] > pivot) {
                 i++;
 
-                // Swap arr[i] and arr[j]
-                double temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                // Swap weight[i] and weight[j]
+                double tempWeight = weight[i];
+                weight[i] = weight[j];
+                weight[j] = tempWeight;
 
-                ArrayList<Item> recipeTemp = recipes.get(i);
+                // Swap recipes.get(i) and recipes.get(j)
+                List<GroceryItemRecipeDTO> tempRecipe = recipes.get(i);
                 recipes.set(i, recipes.get(j));
-                recipes.set(j, recipeTemp);
-
+                recipes.set(j, tempRecipe);
             }
         }
-                // Swap arr[i + 1] and arr[high]
-        double temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
 
-        ArrayList<Item> recipeTemp = recipes.get(i + 1);
+        // Swap weight[i + 1] and weight[high]
+        double tempWeight = weight[i + 1];
+        weight[i + 1] = weight[high];
+        weight[high] = tempWeight;
+
+        // Swap recipes.get(i + 1) and recipes.get(high)
+        List<GroceryItemRecipeDTO> tempRecipe = recipes.get(i + 1);
         recipes.set(i + 1, recipes.get(high));
-        recipes.set(high, recipeTemp);
+        recipes.set(high, tempRecipe);
 
         return i + 1;
     }
-     */
+
+
+
 
 
     }
