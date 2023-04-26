88eb328c876817e1eff24e5cfbd514b2d4f516e9
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-26 13:42:47 +0200

    Added method for updating a fridge. Removed deprecated code.

--- src/main/java/edu/ntnu/idatt2106_09/backend/controller/FridgeController.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/controller/FridgeController.java
@@ -1,17 +1,16 @@
 package edu.ntnu.idatt2106_09.backend.controller;
 
 import edu.ntnu.idatt2106_09.backend.dto.FridgeDto;
-import edu.ntnu.idatt2106_09.backend.exceptionHandling.BadRequestException;
 import edu.ntnu.idatt2106_09.backend.exceptionHandling.NotFoundException;
 import edu.ntnu.idatt2106_09.backend.model.Fridge;
 import edu.ntnu.idatt2106_09.backend.service.fridge.FridgeService;
-import edu.ntnu.idatt2106_09.backend.service.household.HouseholdService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -30,35 +29,27 @@ public class FridgeController {
         return new ResponseEntity<>(newFridge, HttpStatus.CREATED);
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<Set<Fridge>> getAllFridges() {
-        log.debug("[X] Fetching all fridges");
-        Set<Fridge> fridges = fridgeService.getAllFridges();
-        log.debug("[X] Total number of fridges retrieved: {}", fridges.size());
-        return new ResponseEntity<>(fridges, HttpStatus.OK);
+    @PutMapping("/update")
+    public ResponseEntity<FridgeDto> updateFridge(@RequestBody FridgeDto fridgeDto) {
+        log.debug("[X] Updating fridge with id: {}", fridgeDto.getFridgeId());
+        FridgeDto updatedFridge = fridgeService.updateFridge(fridgeDto);
+        log.info("[X] Fridge with id: {} updated", updatedFridge.getFridgeId());
+        return new ResponseEntity<>(updatedFridge, HttpStatus.OK);
     }
 
-    // In FridgeController
+    @GetMapping("/{id}")
+    public ResponseEntity<FridgeDto> getFridgeById(@PathVariable Long id) {
+        log.debug("[X] Received request to get Fridge by id: {}", id);
+        FridgeDto fridgeDto = fridgeService.getFridgeById(id);
+        log.info("Fridge found with id: {} and details: {}", id, fridgeDto);
+        return ResponseEntity.ok(fridgeDto);
+    }
+
     @DeleteMapping("/{fridgeId}")
     public ResponseEntity<Void> deleteFridge(@PathVariable Long fridgeId) {
         log.debug("[X] Deleting fridge with id: {}", fridgeId);
-        Fridge fridge = fridgeService.getFridgeById(fridgeId)
-                .orElseThrow(() -> new NotFoundException("Fridge with id " + fridgeId + " not found for deletion"));
         fridgeService.deleteFridge(fridgeId);
         log.info("[X] Fridge with id {} deleted", fridgeId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
-    /**
-    @DeleteMapping("/{fridgeId}")
-    public ResponseEntity<Void> deleteFridge(@PathVariable Long fridgeId) {
-        log.debug("[X] Deleting fridge with id: {}", fridgeId);
-        Fridge fridge = fridgeService.getFridgeById(fridgeId)
-                .orElseThrow(() -> new NotFoundException("Fridge with id " + fridgeId + " not found for deletion"));
-        fridgeService.deleteFridge(fridgeId);
-        log.info("[X] Fridge with id {} deleted", fridgeId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-    */
 }
