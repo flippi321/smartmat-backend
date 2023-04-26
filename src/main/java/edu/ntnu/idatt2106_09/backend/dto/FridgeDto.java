fe5fb94e5a43f3860af5dbf5f8c8ba2bd23fab2a
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-26 13:43:36 +0200

    Removed list of groceries in DTO object

--- src/main/java/edu/ntnu/idatt2106_09/backend/dto/FridgeDto.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/dto/FridgeDto.java
@@ -6,18 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class FridgeDto {
-
     private Long fridgeId;
     private String name;
     private HouseholdDto household;
-    private Set<GroceryItemDto> groceries = new HashSet<>();
 }
