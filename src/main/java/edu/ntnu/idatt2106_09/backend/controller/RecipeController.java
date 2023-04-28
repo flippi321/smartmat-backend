d29a626007e7895360f76d66d53bf2e153a26a4f
Arnold <arnold01@live.no>
2023-04-28 16:06:30 +0200

    Added Swagger Documentation for RecipeController

--- src/main/java/edu/ntnu/idatt2106_09/backend/controller/RecipeController.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/controller/RecipeController.java
@@ -6,6 +6,8 @@ import edu.ntnu.idatt2106_09.backend.dto.recipe.RecipeResponseDTO;
 import edu.ntnu.idatt2106_09.backend.exceptionHandling.NotFoundException;
 import edu.ntnu.idatt2106_09.backend.model.Recipe;
 import edu.ntnu.idatt2106_09.backend.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,10 @@ import java.util.stream.Collectors;
 
 
 @Slf4j
+@Tag(name = "Recipe Controller", description = "Recipe management operations")
 @RestController
 @RequestMapping("/api/recipes")
+
 public class RecipeController {
 
     @Autowired
@@ -30,6 +34,7 @@ public class RecipeController {
 
 
     @PostMapping
+    @Operation(summary = "Add a new recipe", description = "Add a new recipe to the database")
     public ResponseEntity<RecipeDTO> addRecipe(@RequestBody RecipeDTO recipeDTO) {
 
         Recipe recipe = modelMapper.map(recipeDTO, Recipe.class);
@@ -38,6 +43,7 @@ public class RecipeController {
     }
 
     @GetMapping("/simplified/{recipeId}")
+    @Operation(summary = "Get a simplified recipe by ID", description = "Get a simplified recipe with the specified ID")
     public ResponseEntity<RecipeDTO> getSimplifiedRecipeById(@PathVariable Long recipeId) {
         log.debug("Fetching recipe with id: {}", recipeId);
         Recipe recipe = recipeService.getRecipeById(recipeId)
@@ -48,12 +54,14 @@ public class RecipeController {
     }
 
     @GetMapping("/{recipeId}")
+    @Operation(summary = "Get a recipe by ID", description = "Get a recipe with the specified ID along with all its ingredients")
     public ResponseEntity<RecipeResponseDTO> getRecipeById(@PathVariable Long recipeId) {
         RecipeResponseDTO responseDTO = recipeService.getRecipeAndAllIngredients(recipeId);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @GetMapping
+    @Operation(summary = "Get all recipes", description = "Get all recipes stored in the database")
     public ResponseEntity<Set<RecipeDTO>> getAllRecipes() {
         Set<Recipe> recipes = recipeService.getAllRecipe();
         Set<RecipeDTO> recipeDTOs = recipes.stream()
@@ -64,12 +72,14 @@ public class RecipeController {
 
 
     @DeleteMapping("/{recipeId}")
+    @Operation(summary = "Delete a recipe by ID", description = "Delete a recipe with the specified ID from the database")
     public ResponseEntity<Void> deleteRecipe(@PathVariable Long recipeId) {
         recipeService.deleteRecipe(recipeId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/recommender/{fridgeId}")
+    @Operation(summary = "Get a list of recommended recipes", description = "Get a list of recommended recipes based on the contents of a fridge")
     public ResponseEntity<List<RecipeResponseDTO>> getRecipeRecommendedList(@PathVariable Long fridgeId) {
         // Retrieve the List<List<GroceryItemRecipeDTO>> based on the fridgeId (implement this in your service layer)
         List<List<GroceryItemRecipeDto>> listOfGroceryItemRecipeLists = recipeService.getRecommendedRecipes(fridgeId);
