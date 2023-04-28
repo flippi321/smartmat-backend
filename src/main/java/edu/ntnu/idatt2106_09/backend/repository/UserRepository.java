9e882ebb161b529408514cb7b6979a4115c41b5e
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-04-28 16:15:41 +0200

    Added UserRepository and logic to find a user by email

---
+++ src/main/java/edu/ntnu/idatt2106_09/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2106_09.backend.repository;
+
+import java.util.Optional;
+
+import edu.ntnu.idatt2106_09.backend.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
