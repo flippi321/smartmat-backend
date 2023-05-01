10ccedb6e25caeb96322d100fe97ca3de05e21ce
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-01 13:47:00 +0200

    Added all methods in the 'AuthenticationController' to a whitelist for requests in the security configuration

--- src/main/java/edu/ntnu/idatt2106_09/backend/configuration/SecurityConfiguration.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/configuration/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable() // Disabling cross-site scripting.
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
