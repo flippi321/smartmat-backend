152d94dbc72c15c8fcf7cd4d3ad2c079ecbc4df8
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-01 13:49:33 +0200

    Added a demo controller, which is not whitelisted, for testing that the security filter chain is set up properly

---
+++ src/main/java/edu/ntnu/idatt2106_09/backend/controller/DemoController.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2106_09.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world!");
+    }
+}
