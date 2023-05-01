b39333d64219fa47e92b76697eb137a421aac72e
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-01 10:12:42 +0200

    Added a application config class to hold all application configurations. Added logic to find a username by email (username)

---
+++ src/main/java/edu/ntnu/idatt2106_09/backend/config/AppConfig.java
@@ -0,0 +1,30 @@
+package edu.ntnu.idatt2106_09.backend.config;
+
+import edu.ntnu.idatt2106_09.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Holds all the application configurations, such as beans etc.
+ * At start-up Spring will pick up this class and try to implement and inject all the beans that we'll declare within
+ * this application configuration.
+ *
+ */
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final UserRepository repository;
+
+    /**
+     * Fetches and return the username from the database using the UserRepository.
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
