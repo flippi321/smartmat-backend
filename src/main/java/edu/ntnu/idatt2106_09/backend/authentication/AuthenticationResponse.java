654e70743c5c049b7c364e6d8504a6e0e87aff3c
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-01 12:00:00 +0200

    Added class to represent a simple authentication response

---
+++ src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2106_09.backend.authentication;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
