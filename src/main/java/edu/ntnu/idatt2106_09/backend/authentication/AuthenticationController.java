d8d12f7100e6518e5603905c7ccfab5713d43587
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 11:29:42 +0200

    Added POST-method for refresh tokens.

--- src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationController.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationController.java
@@ -1,11 +1,15 @@
 package edu.ntnu.idatt2106_09.backend.authentication;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 @RestController
@@ -23,5 +27,10 @@ public class AuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
         return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
     }
 }
