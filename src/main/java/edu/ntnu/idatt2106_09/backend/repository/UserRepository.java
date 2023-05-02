46a27a5c768e4653f81cb944ee24c0a7d8263097
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 09:42:36 +0200

    Updated id from type 'Long' to type 'Integer' in UserRepository

--- src/main/java/edu/ntnu/idatt2106_09/backend/repository/UserRepository.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/repository/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import edu.ntnu.idatt2106_09.backend.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
 }
