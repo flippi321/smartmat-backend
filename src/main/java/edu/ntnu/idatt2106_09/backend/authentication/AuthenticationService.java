a33ada9549e7c925ab4886e38d6153b4a451fcba
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 15:14:02 +0200

    Naming changes for variables, formatting changes, added TODOs.

--- src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationService.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationService.java
@@ -29,61 +29,15 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-
-    private void createUserToken(User user, String userToken) {
+    private void saveUserTokenToRepository(User user, String accessToken) {
         var token = Token.builder()
                 .user(user)
-                .token(userToken)
+                .token(accessToken)
                 .tokenType(TokenType.BEARER)
                 .revoked(false)
                 .expired(false)
                 .build();
         tokenRepository.save(token);
-    }
-
-    /**
-     * This will allow us to create a user, save it to the database and return the generated token out of it.
-     */
-    public AuthenticationResponse register(RegistrationRequest request) {
-        var user = User.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword())) // Encoding the password before saving it to the database.
-                .role(Role.USER)
-                .build();
-        var savedUser = userRepository.save(user); // Saving the user to the repository.
-        var generatedAccessToken = jwtService.generateAccessToken(user); // Creating an access token.
-        var generatedRefreshToken = jwtService.generateRefreshToken(user); // Creating a refresh token.
-        createUserToken(savedUser, generatedAccessToken);
-
-        return AuthenticationResponse.builder()
-                // Returning an access token after the registration to make it easy for the user, not making it
-                // necessary to reconnect to have an access token to use. This makes it easy to for example directly
-                // redirect a user when they register.
-                .accessToken(generatedAccessToken)
-                .refreshToken(generatedRefreshToken)
-                .build();
-    }
-
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        // An exception will be thrown if the username (email) or password is wrong.
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-
-        // At this point the user is authenticated.
-
-        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
-        var generatedAccessToken = jwtService.generateAccessToken(user); // Creating an access token.
-        var generatedRefreshToken = jwtService.generateRefreshToken(user); // Creating a refresh token.
-        revokeAllTokensForUser(user);
-        createUserToken(user, generatedAccessToken);
-
-        return AuthenticationResponse.builder()
-                .accessToken(generatedAccessToken)
-                .refreshToken(generatedRefreshToken)
-                .build();
     }
 
     private void revokeAllTokensForUser(User user) {
@@ -100,26 +54,68 @@ public class AuthenticationService {
         tokenRepository.saveAll(validUserTokens);
     }
 
+    /**
+     * This will allow us to create a user, save it to the database and return the generated token out of it.
+     */
+    public AuthenticationResponse register(RegistrationRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        var savedUser = userRepository.save(user);
+        var generatedAccessToken = jwtService.generateAccessToken(user);
+        var generatedRefreshToken = jwtService.generateRefreshToken(user);
+        saveUserTokenToRepository(savedUser, generatedAccessToken);
+
+        return AuthenticationResponse.builder()
+                // Returning an access token after the registration to make it easy for the user - not making it
+                // necessary to reconnect to have an access token to use. This makes it easy to for example directly
+                // redirect a user when they register.
+                .accessToken(generatedAccessToken)
+                .refreshToken(generatedRefreshToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        // TODO throw exception if the username (email) or password is wrong.
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        // At this point the user is authenticated.
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var generatedAccessToken = jwtService.generateAccessToken(user);
+        var generatedRefreshToken = jwtService.generateRefreshToken(user);
+        revokeAllTokensForUser(user);
+        saveUserTokenToRepository(user, generatedAccessToken);
+
+        return AuthenticationResponse.builder()
+                .accessToken(generatedAccessToken)
+                .refreshToken(generatedRefreshToken)
+                .build();
+    }
+
     public void handleTokenRefreshRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        final String refreshToken;
-        final String userEmail;
-
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             return;
         }
 
-        refreshToken = authorizationHeader.substring(7);
-        userEmail = jwtService.extractUsername(refreshToken);
-
+        final String refreshToken = authorizationHeader.substring(7);
+        final String userEmail = jwtService.extractUsername(refreshToken);
         if (userEmail != null) {
             var user = this.userRepository.findByEmail(userEmail).orElseThrow();
-
             // TODO Should the refresh token also be revoked?
+            // TODO Check for token expiration before using it?
+            // TODO Add logic instead of just using 'orElseThrow()'?
             if (jwtService.isTokenValid(refreshToken, user)) {
                 var generatedAccessToken = jwtService.generateAccessToken(user);
                 revokeAllTokensForUser(user);
-                createUserToken(user, generatedAccessToken);
+                saveUserTokenToRepository(user, generatedAccessToken);
                 var authenticationResponse = AuthenticationResponse.builder()
                         .accessToken(generatedAccessToken)
                         .refreshToken(refreshToken)
