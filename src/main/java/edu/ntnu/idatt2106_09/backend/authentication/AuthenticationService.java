d5065b9d07f6fbd3f85132ffb5d8b5ab0e45dc77
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 09:39:20 +0200

    Extracted method for creating and saving a token based on a user. Added method for revoking all tokens from a user.

--- src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationService.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationService.java
@@ -2,6 +2,9 @@ package edu.ntnu.idatt2106_09.backend.authentication;
 
 import edu.ntnu.idatt2106_09.backend.configuration.JwtService;
 import edu.ntnu.idatt2106_09.backend.repository.UserRepository;
+import edu.ntnu.idatt2106_09.backend.token.Token;
+import edu.ntnu.idatt2106_09.backend.token.TokenRepository;
+import edu.ntnu.idatt2106_09.backend.token.TokenType;
 import edu.ntnu.idatt2106_09.backend.user.Role;
 import edu.ntnu.idatt2106_09.backend.user.User;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +18,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+
+
+    private void createUserToken(User user, String userToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(userToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
+    }
 
     /**
      * This will allow us to create a user, save it to the database and return the generated token out of it.
@@ -31,29 +47,43 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
         var savedUser = userRepository.save(user); // Saving the user to the repository.
-        var token = jwtService.generateToken(user); // Creating a token using the 'user' object.
+        var generatedToken = jwtService.generateToken(user); // Creating a token using the 'user' object.
+        createUserToken(savedUser, generatedToken);
 
         return AuthenticationResponse.builder()
-                .token(token)
+                .token(generatedToken)
                 .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         // An exception will be thrown if the username (email) or password is wrong.
         authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
         );
 
         // At this point the user is authenticated.
 
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
-        var token = jwtService.generateToken(user);
+        var generatedToken = jwtService.generateToken(user);
+        revokeAllTokensForUser(user);
+        createUserToken(user, generatedToken);
+
         return AuthenticationResponse.builder()
-                .token(token)
+                .token(generatedToken)
                 .build();
     }
 
+    private void revokeAllTokensForUser(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty()) {
+            return;
+        }
+
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+
+        tokenRepository.saveAll(validUserTokens);
+    }
 }
