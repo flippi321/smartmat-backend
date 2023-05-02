b59139e4dfd6e7645d144f72c311f440b62e10e8
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 15:15:46 +0200

    Removed comments

--- src/main/java/edu/ntnu/idatt2106_09/backend/config/JwtService.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/config/JwtService.java
@@ -18,8 +18,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-    // For security reasons we need at the minimum a sign-in key with a size of minimum 256 bits.
-    //private static final String SECRET_KEY = "48404D6351665468576D5A7134743777217A25432A462D4A614E645267556B58";
     @Value("${application.security.jwt.secret-key}")
     private String secretKey;
     @Value("${application.security.jwt.expiration}")
@@ -39,12 +37,6 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
-                // In the context of jwt, a sign in key is a secret that is used to digitally sign the jwt. It's used to
-                // create the signature part of the jwt, which is used to verify that the sender of the jwt is who he/she
-                // claims to be and that the message wasn't changed along the way.
-                // The sign in key is used in conjunction with the sign in algorithm, specified in the jwt header to create
-                // the signature. The specific sign-in algorithm and key-size will depend on the security requirement of
-                // your application and the level of trust you have in the sign-in party.
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
@@ -55,11 +47,8 @@ public class JwtService {
         return generateAccessToken(new HashMap<>(), userDetails);
     }
 
-    /**
-     *
-     * @param extraClaims If you want to pass extra claims, for example: authorities, any information you want to store
-     *                    within the token.
-     */
+    // 'extraClaims' is if you want to pass extra claims, for example: authorities, any information you want to store
+    // within the token.
     public String generateAccessToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return buildToken(extraClaims, userDetails, accessTokenExpiration);
     }
@@ -76,7 +65,7 @@ public class JwtService {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact(); // Generates and returns the token.
+                .compact();
     }
 
     /**
@@ -97,7 +86,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey); // Base64 decoding the secret key.
-        return Keys.hmacShaKeyFor(keyBytes); // Using a hashing-algorithm to hash the secret key.
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
