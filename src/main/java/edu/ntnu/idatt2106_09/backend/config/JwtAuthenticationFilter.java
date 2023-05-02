c6493789be92a438a0a82b4f867d64c42487a8a3
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 15:15:21 +0200

    Formatting changes, removed comments

--- src/main/java/edu/ntnu/idatt2106_09/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/config/JwtAuthenticationFilter.java
@@ -17,9 +17,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-// Tells Spring that this class should be a Spring Bean.
 @Component
-// Will create a constructor which takes in all class variables as arguments.
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -28,45 +26,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final TokenRepository tokenRepository;
 
     @Override
-    protected void doFilterInternal(
-            // We can intercept every request and for example extract data from it and provide new data within the
-            // response. This could be that we wanted to add a header to the response.
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            // The chain-of-responsibility pattern. It contains a list of all the filters we want to execute.
-            // If you call filterchain.doFilter() it will call the next filter within the chain.
-            @NonNull FilterChain filterChain
-    ) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        // When we make a call, we need to pass the token token within the header. Under, we're extracting this header.
+        // When we make a call, the token that lies within the header must be passed. Extracting that below.
         final String authorizationHeader = request.getHeader("Authorization");
-        final String token;
-        final String userEmail;
-
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response); // Passing the request and response to the next filter.
-            return; // Stops the execution of the method.
+            return;
         }
 
-        // Extracting the token from the auth-header.
-        token = authorizationHeader.substring(7); // Starting from pos no. 7 as we're skipping 'Bearer '.
-        // Extracting the user email from the token.
-        userEmail = jwtService.extractUsername(token);
-
-        // Checks that the user exists and that it's not already authenticated yet. Because if the user is authenticated,
-        // we don't need to perform all the checks in the if-sentence again.
+        final String token = authorizationHeader.substring(7); // Starting from pos no. 7 as we're skipping 'Bearer '.
+        final String userEmail = jwtService.extractUsername(token);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-
             var isTokenValid = tokenRepository.findByToken(token)
                     .map(t -> !t.isExpired() && !t.isRevoked())
                     .orElse(false);
-
             if(jwtService.isTokenValid(token, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
@@ -78,7 +60,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }
 
-        // Passing the hand to the next filter to be executed.
         filterChain.doFilter(request, response);
     }
 }
