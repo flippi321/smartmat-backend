3ff11ffb088d9e02d880f7f55f5edf5f438cf794
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-02 09:41:53 +0200

    Added logic to check if a token is valid via the token repository. This ensures that a user cannot use an old token to authenticate or access secured endpoints.

--- src/main/java/edu/ntnu/idatt2106_09/backend/configuration/JwtAuthenticationFilter.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/configuration/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2106_09.backend.configuration;
 
+import edu.ntnu.idatt2106_09.backend.token.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,6 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -60,7 +62,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // we don't need to perform all the checks in the if-sentence again.
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if(jwtService.isTokenValid(token, userDetails)) {
+
+            var isTokenValid = tokenRepository.findByToken(token)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+
+            if(jwtService.isTokenValid(token, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
