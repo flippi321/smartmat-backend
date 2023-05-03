3aa8e91c4c5e360fbd8d2519ce2af79ae3adbe48
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-03 14:13:54 +0200

    Added overridden toString to 'User' class as we don't want to expose the hashed password or tokens in logging statements

--- src/main/java/edu/ntnu/idatt2106_09/backend/user/User.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/user/User.java
@@ -69,4 +69,15 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
