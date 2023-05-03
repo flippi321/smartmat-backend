3f3c7d1183f118a2768fd4424a974e8406e89505
Arnold <arnold01@live.no>
2023-05-03 11:03:22 +0200

    Added some more swagger documentation for RecipeController

--- src/main/java/edu/ntnu/idatt2106_09/backend/controller/RecipeController.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/controller/RecipeController.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2106_09.backend.controller;
 
+import edu.ntnu.idatt2106_09.backend.dto.FridgeDto;
 import edu.ntnu.idatt2106_09.backend.dto.GroceryItemRecipeDto;
 import edu.ntnu.idatt2106_09.backend.dto.recipe.RecipeDTO;
 import edu.ntnu.idatt2106_09.backend.dto.recipe.RecipeResponseDTO;
@@ -7,6 +8,9 @@ import edu.ntnu.idatt2106_09.backend.exceptionHandling.NotFoundException;
 import edu.ntnu.idatt2106_09.backend.model.Recipe;
 import edu.ntnu.idatt2106_09.backend.service.RecipeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -36,6 +40,9 @@ public class RecipeController {
 
     @PostMapping
     @Operation(summary = "Add a new recipe", description = "Add a new recipe to the database")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Invalid json object provided")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<Object> addRecipe(@RequestBody RecipeDTO recipeDTO) {
         log.debug("Adding a new Recipe named: {} ", recipeDTO.getName());
         return recipeService.addRecipe(recipeDTO);
@@ -55,11 +62,15 @@ public class RecipeController {
 
     @GetMapping("/{recipeId}")
     @Operation(summary = "Get a recipe by ID", description = "Get a recipe with the specified ID along with all its ingredients")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    @ApiResponse(responseCode = "404", description = "Recipe not found")
     public ResponseEntity<Object> getRecipeById(@PathVariable Long recipeId) {
         return recipeService.getRecipeAndAllIngredients(recipeId);
     }
 
     @GetMapping
+    @ApiResponse(responseCode = "200", description = "OK")
     @Operation(summary = "Get all recipes", description = "Get all recipes stored in the database")
     public ResponseEntity<Set<RecipeDTO>> getAllRecipes() {
         log.debug("Retrieving all recipes");
@@ -68,6 +79,9 @@ public class RecipeController {
 
 
     @DeleteMapping("/{recipeId}")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    @ApiResponse(responseCode = "404", description = "Recipe not found")
     @Operation(summary = "Delete a recipe by ID", description = "Delete a recipe with the specified ID from the database")
     public ResponseEntity<Object> deleteRecipe(@PathVariable Long recipeId) {
         log.debug("Deleting Recipe with id:" + recipeId);
@@ -75,7 +89,7 @@ public class RecipeController {
     }
 
     @GetMapping("/recommender/{fridgeId}")
-    @Operation(summary = "Get a list of recommended recipes", description = "Get a list of recommended recipes based on the contents of a fridge")
+    @Operation(summary = "Get a recommended recipe", description = "Receive a Recipe based on the contents of the fridge")
     public ResponseEntity<List<RecipeResponseDTO>> getRecipeRecommendedList(@PathVariable Long fridgeId) {
         // Retrieve the List<List<GroceryItemRecipeDTO>> based on the fridgeId (implement this in your service layer)
         List<List<GroceryItemRecipeDto>> listOfGroceryItemRecipeLists = recipeService.getRecommendedRecipes(fridgeId);
@@ -84,6 +98,10 @@ public class RecipeController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
     @GetMapping("/weekRecommender/{fridgeId}")
+    @Operation(summary = "Get a list of recommended recipes", description = "Get a list of recommended recipes based on the contents of a fridge")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    @ApiResponse(responseCode = "404", description = "Fridge not found")
     public ResponseEntity<Object> getRecommendedWeekMenuList(@PathVariable Long fridgeId) {
         try{
             List<List<GroceryItemRecipeDto>> listOfGroceryItemRecipeLists = recipeService.retrieveRecommendedWeekMenu(fridgeId);
@@ -97,6 +115,10 @@ public class RecipeController {
     }
 
     @GetMapping("/missingIngredients/{fridgeId}/{recipeId}")
+    @Operation(summary = "Get missing ingredients of a recipe", description = "Returns two items in list. One represents the missing ingredients, while the other is the original recipe")    @ApiResponse(responseCode = "200", description = "Fridge updated successfully", content = @Content(schema = @Schema(implementation = FridgeDto.class)))
+    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    @ApiResponse(responseCode = "404", description = "Fridge or Recipe not found")
     public ResponseEntity<Object> getMissingIngredients(@PathVariable Long fridgeId, @PathVariable Long recipeId) {
         try{
             List<RecipeResponseDTO> responseDto = recipeService.getMissingIngredientsAndOriginalRecipe(fridgeId,recipeId);
