d60c293a159329f95915b89444a781e214df7fb7
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-04 10:47:12 +0200

    Added logging statements to 'register' and 'authenticate' in the AuthenticationController

--- src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationController.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/authentication/AuthenticationController.java
@@ -3,11 +3,13 @@ package edu.ntnu.idatt2106_09.backend.authentication;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -17,12 +19,18 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegistrationRequest registrationRequest) {
-        return ResponseEntity.ok(authenticationService.register(registrationRequest));
+        log.debug("[X] Received registration request for email: {}", registrationRequest.getEmail());
+        AuthenticationResponse authenticationResponse = authenticationService.register(registrationRequest);
+        log.info("[X] Registration request successful for email: {}", registrationRequest.getEmail());
+        return ResponseEntity.ok(authenticationResponse);
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
-        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+        log.debug("[X] Received authentication request for email: {}", authenticationRequest.getEmail());
+        AuthenticationResponse authenticationResponse = authenticationService.authenticate(authenticationRequest);
+        log.info("[X] Authentication request successful for email: {}", authenticationRequest.getEmail());
+        return ResponseEntity.ok(authenticationResponse);
     }
 
     @PostMapping("/refresh-token")
