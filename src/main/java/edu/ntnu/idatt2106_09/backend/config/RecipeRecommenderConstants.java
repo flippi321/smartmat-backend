bda7bb4cbae381c417175c3ad8a9b6fe55fe95af
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2023-05-04 22:23:32 +0200

    Added javadoc

--- src/main/java/edu/ntnu/idatt2106_09/backend/config/RecipeRecommenderConstants.java
+++ src/main/java/edu/ntnu/idatt2106_09/backend/config/RecipeRecommenderConstants.java
@@ -2,6 +2,9 @@ package edu.ntnu.idatt2106_09.backend.config;
 
 import lombok.NoArgsConstructor;
 
+/**
+ * A class containing constants used by the RecipeRecommender class.
+ */
 @NoArgsConstructor
 public class RecipeRecommenderConstants {
     public static final double RECOMMENDATION_THRESHOLD = 0.2;
